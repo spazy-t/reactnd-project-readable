f280ab9eea71a11943d8de239788d57461d8f623
Tom Schrimshaw <tschrimshaw@gmail.com>
2020-10-21 15:08:03 +0100

    feat: selector to sort posts

--- frontend/src/containers/CatContainer.js
+++ frontend/src/containers/CatContainer.js
@@ -1,16 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { connect } from 'react-redux'
 import PostsCard from './PostsCard'
 import { withRouter, Link, useParams } from 'react-router-dom'
 import FourOFour from '../screens/FourOFour'
 import { StyledCategory } from '../styles/main'
 
-const CatContainer = (props) => {
-    const { id, postsForCat, history, categories } = props
+const CatContainer =  (props) => {
+    let orderedPosts= []
+    const { id, postsForCat, allPosts, history, categories } = props
     //grabs parameter on url if there, if so then we are in individual category screen
     const { category }  = useParams()
+    //local state to determine posts sort order
+    const [sortOrder, setSort] = useState('voteScore')
 
-    //TODO: find was of testing the categories length as an obj not array, as as an array it'll only take an index number not a named parameter
+    //https://stackoverflow.com/questions/1069666/sorting-object-property-by-values (21/10/2020)
+    //determine the sort order of the posts via local state obj
+    const orderPosts = () => {
+        orderedPosts = postsForCat.sort((a, b) => {
+            return allPosts[b][sortOrder] - allPosts[a][sortOrder]
+        })
+    }
+
+    orderPosts()
+
+    //if not on root screen and we have gotten categories from store but url points to unknown category, show 404
     if(category !== undefined && Object.keys(categories).length !== 0 && categories[category] === undefined) {
         return <FourOFour />
     }
@@ -25,10 +38,13 @@ const CatContainer = (props) => {
                 </Link>
                 : <h2>{id}</h2>
             }
-            
+            <select onChange={(evt) => setSort(evt.target.value)}>
+                <option value='voteScore'>Order by Score</option>
+                <option value='timestamp'>Order by Date</option>
+            </select>
             <section>
-                {postsForCat.length !== 0
-                    ? postsForCat.map(post => (
+                {orderedPosts.length !== 0
+                    ? orderedPosts.map(post => (
                         <PostsCard key={ post } id={ post } />
                     ))
                     : `No posts currently!`
@@ -39,7 +55,6 @@ const CatContainer = (props) => {
     )
 }
 
-//TODO: for categories bring in the object from state, not an array
 //filters out the posts that have the same category as for this container, then grabs it's id to pass to PostCard component
 function mapStateToProps({ posts, categories }, { id }) {
     return {
@@ -47,7 +62,8 @@ function mapStateToProps({ posts, categories }, { id }) {
         postsForCat: Object.keys(posts).filter(post => posts[post].category === id)
             .map(catPost => (
                 posts[catPost].id
-            ))
+            )),
+        allPosts: posts
     }
 }
 
